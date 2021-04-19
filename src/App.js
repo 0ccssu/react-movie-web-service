f23117c54936150108fcd3a4f8ccb6e1e6d600b0
Achelous1 <jsdesign1204@gmail.com>
2021-04-19 11:15:58 +0900

    Complete #2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Potato from './Potato';  // 해당 컴퍼넌트 내에서 사용되는 Potato 컴퍼넌트 임포트
 
-// App Element
+// <App/> 태그로 HTML을 반환하는 컴퍼넌트
 function App() {
-  return <div><h1>Hello!</h1></div>;
+  return <div>
+            <h1>Hello!</h1>
+            <Potato />
+         </div>;
 }
 
 export default App;
