ce466b0da35b648c503bdc875cea77c574a13c88
Achelous1 <jsdesign1204@gmail.com>
2021-04-19 16:46:00 +0900

    Complete #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Potato from './Potato';  // 해당 컴퍼넌트 내에서 사용되는 Potato 컴퍼넌트 임포트 **reference from #2.0
+import PropTypes from "prop-types";
 
 /**
  * #2.1 Reusable Components
@@ -24,45 +25,65 @@ const foodILike = [{
   id: 1,
   name: "Kimchi",
   image:
-    "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4
 },
 {
   id: 2,
   name: "Samgyeopsal",
   image:
-    "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+    "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+  rating: 5
 },
 {
   id: 3,
   name: "Bibimbap",
   image:
-    "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+    "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.3
 },
 {
   id: 4,
   name: "Doncasu",
   image:
-    "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+    "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5
 },
 {
   id: 5,
   name: "Kimbap",
   image:
-    "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+    "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4
 }];
 
-function Food ({ name, picture }) { // 특정 속성값을 인자로 받고 싶을 때
+function Food ({ name, picture, rating}) { // 특정 속성값을 인자로 받고 싶을 때
   console.log(name);
   return (
     <div>
       <h3>I like {name}</h3>
+      <h4>{rating} / 5.0</h4>
       <img src={picture} alt={name}/>
     </div>
   )
 }
 
 function renderFood(dish){
-  return <Food key={dish.id} name={dish.name} picture={dish.image}/>  // key prop은 리액트 내부에서 사용하기 위한 prop이다
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>  // key prop은 리액트 내부에서 사용하기 위한 prop이다
+}
+
+/**
+ * reference from #2.4 
+ * prop validation by prop-types plugin
+ * <a href="https://reactjs.org/docs/typechecking-with-proptypes.html">
+ * Refer to
+ * </a>
+ * <b>important!</b> propTypes 이외의 명으로 필드명을 지을 수 없다
+ */
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
 }
 
 // <App/> 태그로 HTML을 반환하는 컴퍼넌트
