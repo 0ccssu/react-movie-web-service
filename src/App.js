9d606cc8951445c0ecabd664b463d83c807b2635
Achelous1 <jsdesign1204@gmail.com>
2021-04-21 16:03:55 +0900

    Complete #4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import PropTypes from "prop-types";
 import axios from "axios";
+import Movie from "./Movie";
 
 // Function -> Class component 로 변환(React.Component를 상속)
 // Component의 render 메서드와 state를 상속
@@ -14,7 +14,13 @@ class App extends React.Component {
   getMovies = async () => {
     // axios 라이브러리를 사용하여 영화리스트를 get 한다
     // async와 await 힌트를 사용하여 비동기처리를 한다.(둘은 같이 가야함)
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const { 
+      data: { 
+        data: { movies } 
+      } 
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount = () => {
@@ -22,11 +28,20 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
 
     return (
       <div>
-        {isLoading ? "Loading..." : "We are ready"}
+        {isLoading ? "Loading..." : movies.map(movie => {
+          console.log(movie);
+          return <Movie key={movie.id}
+                        id={movie.id} 
+                        year={movie.year} 
+                        title={movie.title} 
+                        summary={movie.summary} 
+                        poster={movie.medium_cover_image} 
+                  />
+        })}
       </div>
     )
   }
---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// state를 필요로 하지 않을때는 class로 정의하지 않아도 된다
+function Movie({id, year, title, summary, poster}) {
+    return <h4>{title}</h4>
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+export default Movie;
