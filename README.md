e5562ec3030ed3926075e628d993601ad63b85e1
Achelous1 <jsdesign1204@gmail.com>
2021-04-21 14:52:16 +0900

    Complete #4.0 Fetching Movies from API

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 * [NPX](https://medium.com/@maybekatz/introducing-npx-an-npm-package-runner-55f7d4bd282b#:~:text=npx%20is%20a%20tool%20intended,executables%20hosted%20on%20the%20registry.) 설치 `npm install npx -g`
 * `npx create-react-app {프로젝트명}`으로 리액트 앱 생성
 * 생성 된 파일 중 `package.json`파일 내부의 "scripts" 항목 중 `test`와 `eject` 항목 삭제
+* `prop-types` 라이브러리 설치 `npm i prop-types`
+* `axios` 라이브러리 설치 `npm i axios`
 
 ## :thumbsup: Cool thing about ReactJS
 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import axios from "axios";
 
 // Function -> Class component 로 변환(React.Component를 상속)
 // Component의 render 메서드와 state를 상속
@@ -10,10 +11,14 @@ class App extends React.Component {
     movies: []
   };
 
+  getMovies = async () => {
+    // axios 라이브러리를 사용하여 영화리스트를 get 한다
+    // async와 await 힌트를 사용하여 비동기처리를 한다.(둘은 같이 가야함)
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
+
   componentDidMount = () => {
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
