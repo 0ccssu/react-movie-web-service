4a29c0c3d93f8e9213f74f3a5b00a9635cba11d9
Achelous1 <jsdesign1204@gmail.com>
2021-04-21 16:25:38 +0900

    Complete #4.3 Adding Genres

--- src/App.js
+++ src/App.js
@@ -32,13 +32,13 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
 
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-        <div class="loader">
-          <span class="loader__text">Loading...</span>
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
         </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map(movie => {
               console.log(movie);
               return <Movie key={movie.id}
@@ -46,7 +46,8 @@ class App extends React.Component {
                             year={movie.year} 
                             title={movie.title} 
                             summary={movie.summary} 
-                            poster={movie.medium_cover_image} 
+                            poster={movie.medium_cover_image}
+                            genres={movie.genres}
                       />
             })}
           </div>
--- src/Movie.js
+++ src/Movie.js
@@ -3,14 +3,18 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 // state를 필요로 하지 않을때는 class로 정의하지 않아도 된다
-function Movie({id, year, title, summary, poster}) {
+function Movie({id, year, title, summary, poster, genres}) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title}/>
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="genres"> {genres.map((genre, idx) => 
+                    <li key={idx} className="genres__name">{genre}</li>
+                )}
+                </ul>
+                <p className="movie__summary">{summary}</p>
             </div>
         </div>
     )
@@ -21,7 +25,8 @@ Movie.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default Movie;
